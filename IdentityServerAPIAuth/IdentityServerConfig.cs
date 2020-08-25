710fea5fda5d0f31c123d3dd26820c0b66edd6c1
Manasa Manchikanti <manchikantim@angloeastern.com>
2020-08-25 03:27:40 +0000

    MM - Added PROD client credentials for NS, JiBe & SB Dev team
    git-tfs-id: [https://dev.azure.com/AESMDEV/]$/AE Dev Project/API Auth Module/Main/IdentityServerAPIAuth;C3313

--- IdentityServerAPIAuth/IdentityServerConfig.cs
+++ IdentityServerAPIAuth/IdentityServerConfig.cs
@@ -18,36 +18,69 @@ namespace IdentityServerAPIAuth
         {
             return new List<Client>
             {
+                //for Stagebox dev team testing
                 new Client
                 {
-                    ClientId = "devCCClient",
+                    ClientId = "devCCProdClient",
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = {new Secret("devCCSecret".Sha256()) },
+                    ClientSecrets = {new Secret("devCCProdSecret".Sha256()) },
                     AllowedScopes = { "Stagebox" }
                 },
                 new Client
                 {
-                    ClientId = "devRopClient",
+                    ClientId = "devRopProdClient",
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                    ClientSecrets = {new Secret("devRopsecret".Sha256()) },
+                    ClientSecrets = {new Secret("devRopProdSecret".Sha256()) },
                     AllowOfflineAccess = true,
                     AllowedScopes = { "Stagebox", IdentityServerConstants.StandardScopes.OfflineAccess },
                     UpdateAccessTokenClaimsOnRefresh = true
                 },
+                //PROD Credentials for NS & JiBe
                 new Client
                 {
-                    ClientId = "AESBJB0B17F631177445349BA91F7DE6111CCC",
+                    ClientId = "AESBJB4A917DC34FBD47C0B54BBDE6110668C4",
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = {new Secret("QZJGmZvorFJpUkJIxK5uoLb6s7Wctwhze8XJpIlEQ9vmh0uGYOJ94evQAjxFkrxUGnI3pL".Sha256()) },
+                    ClientSecrets = {new Secret("FJtLIJGuR8TJ9hGD0JtR6ydAZkEnnwzVWeDHQ4bYrfbjizGuCekbg0IAjG1O7rRxUlzHaJ".Sha256()) },
                     AllowedScopes = { "Stagebox" }
                 },
                 new Client
                 {
-                    ClientId = "AESBNS12B0A66C897C4B19B6647B86AF541D77",
+                    ClientId = "AESBNS680838703C4E4F02991E4EC711692BA0",
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = {new Secret("U4QqgRoJz8qWcN3rx3Mz7chabBbzCR73FGaUfRSG8CcwtctsHnrFsVjcLv5paryAaxi7b7".Sha256()) },
+                    ClientSecrets = {new Secret("apVfpvNVF44iriYe9SCI5l0mq7A9snJDZXMeB7paoHXEaIcyujTmAHqw88WoJzs1VVALsu".Sha256()) },
                     AllowedScopes = { "Stagebox" }
                 }
+                //Test, MIG Environments
+                //new Client
+                //{
+                //    ClientId = "devCCClient",
+                //    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                //    ClientSecrets = {new Secret("devCCSecret".Sha256()) },
+                //    AllowedScopes = { "Stagebox" }
+                //},
+                //new Client
+                //{
+                //    ClientId = "devRopClient",
+                //    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                //    ClientSecrets = {new Secret("devRopsecret".Sha256()) },
+                //    AllowOfflineAccess = true,
+                //    AllowedScopes = { "Stagebox", IdentityServerConstants.StandardScopes.OfflineAccess },
+                //    UpdateAccessTokenClaimsOnRefresh = true
+                //},
+                //new Client
+                //{
+                //    ClientId = "AESBJB0B17F631177445349BA91F7DE6111CCC",
+                //    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                //    ClientSecrets = {new Secret("QZJGmZvorFJpUkJIxK5uoLb6s7Wctwhze8XJpIlEQ9vmh0uGYOJ94evQAjxFkrxUGnI3pL".Sha256()) },
+                //    AllowedScopes = { "Stagebox" }
+                //},
+                //new Client
+                //{
+                //    ClientId = "AESBNS12B0A66C897C4B19B6647B86AF541D77",
+                //    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                //    ClientSecrets = {new Secret("U4QqgRoJz8qWcN3rx3Mz7chabBbzCR73FGaUfRSG8CcwtctsHnrFsVjcLv5paryAaxi7b7".Sha256()) },
+                //    AllowedScopes = { "Stagebox" }
+                //}
             };
         }
     }
